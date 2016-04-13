8387abc81de56d88f66f16ed11a4cede42449ba4
pwinter303 <pwinter303@yahoo.com>
2016-04-12 21:40:29 -0400

    filter/criteria

--- app/scripts/controllers/addfilter.js
+++ app/scripts/controllers/addfilter.js
@@ -19,15 +19,20 @@ angular.module('collegeApp')
                 schoolSetting: {rural: 0, city: 50000},
                 testScore: {SAT: 0, ACT: 500, matching:"worst"},
                 home: {zipCode: "02332", minDistanceAway: 50, maxDistanceAway:500},
-                sports: {sportsRequired: "row1women"}
+                sports: {sportsRequired: "row1women", enabled: false}
             }
         });
 
 
-
-        $scope.saveIt = function(formData){
-            var x = formData;
-            //TODO: Call Factory Service And Save via PHP
+    $scope.saveSports = function(){
+      $scope.myForm.sports.enabled = true;
+    }
+    $scope.resetSports = function(){
+      $scope.myForm.sports.enabled = false;
+    }
+    $scope.saveIt = function(formData, func){
+        var x = formData;
+        //TODO: Call Factory Service And Save via PHP
     }
 
   }]);

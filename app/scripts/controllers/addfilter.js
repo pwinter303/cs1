5c5a4d704f9dd50a9066afe638ab73894488306b
pwinter303 <pwinter303@yahoo.com>
2016-04-12 22:15:01 -0400

    refactor of filter/criteria

--- app/scripts/controllers/addfilter.js
+++ app/scripts/controllers/addfilter.js
@@ -19,6 +19,7 @@ angular.module('collegeApp')
                 schoolSetting: {rural: 0, city: 50000},
                 testScore: {SAT: 0, ACT: 500, matching:"worst"},
                 home: {zipCode: "02332", minDistanceAway: 50, maxDistanceAway:500},
+                loc2: {zipCode: "06085", minDistanceAway: 5, maxDistanceAway:50},
                 sports: {sportsRequired: "row1women", enabled: false}
             }
         });
@@ -30,8 +31,24 @@ angular.module('collegeApp')
     $scope.resetSports = function(){
       $scope.myForm.sports.enabled = false;
     }
+    $scope.resetIt = function(formData, func){
+      if (func == 'home'){$scope.myForm.home.enabled = false;}
+      if (func == 'loc2'){$scope.myForm.loc2.enabled = false;}
+      if (func == 'sports'){$scope.myForm.sports.enabled = false;}
+      if (func == 'testScore'){$scope.myForm.testScore.enabled = false;}
+      if (func == 'schoolSetting'){$scope.myForm.schoolSetting.enabled = false;}
+      if (func == 'schoolCost'){$scope.myForm.schoolCost.enabled = false;}
+      if (func == 'schoolSize'){$scope.myForm.schoolSize.enabled = false;}
+      //TODO: Call Factory Service And Save via PHP
+    }
     $scope.saveIt = function(formData, func){
-        var x = formData;
+        if (func == 'home'){$scope.myForm.home.enabled = true;}
+        if (func == 'loc2'){$scope.myForm.loc2.enabled = true;}
+        if (func == 'sports'){$scope.myForm.sports.enabled = true;}
+        if (func == 'testScore'){$scope.myForm.testScore.enabled = true;}
+        if (func == 'schoolSetting'){$scope.myForm.schoolSetting.enabled = true;}
+        if (func == 'schoolCost'){$scope.myForm.schoolCost.enabled = true;}
+        if (func == 'schoolSize'){$scope.myForm.schoolSize.enabled = true;}
         //TODO: Call Factory Service And Save via PHP
     }
 
